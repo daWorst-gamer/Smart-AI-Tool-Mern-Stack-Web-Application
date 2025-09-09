6fca45b09a0ec65861e5ba53b9e7a24ca66833f0
Muzamil Khan <113015136+Zaibten@users.noreply.github.com>
2025-09-09 13:10:19 +0500

    Update index.jsx

--- Web Application/Select AI Tool/src/pages/AIToolsDirectory/index.jsx
+++ Web Application/Select AI Tool/src/pages/AIToolsDirectory/index.jsx
@@ -700,9 +700,11 @@ const ChipView = ({ chips, selectedChips, onChipClick, multiSelect }) => {
 
         {/* Hero Content */}
 <div className="text-center mb-20 sticky top-0 z-40 px-4 py-4 sm:py-6">
-  <h1 className="text-[20px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-extrabold text-white font-['Lexend_Mega'] leading-tight mb-3 sm:mb-7">
-    A Curated List For The Best AI Tools
-  </h1>
+  <h1 className="text-[20px] sm:text-[28px] md:text-[36px] lg:text-[48px] xl:text-[56px] 
+               font-extrabold text-white font-['Lexend_Mega'] leading-snug sm:leading-tight 
+               mb-3 sm:mb-5 text-center">
+  A Curated List For The Best AI Tools
+</h1>
   <div className="max-w-4xl mx-auto">
     <p className="text-base sm:text-lg md:text-xl text-white font-['Public_Sans'] leading-relaxed">
       <span className="font-normal">
